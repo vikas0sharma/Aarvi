901970bbebed5d72ad4ddb1e43270ff55be19c9a
Vikas Sharma <v.sharma@global.ntt>
2025-05-24 14:52:14 +0530

    Add more MCP tools

--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
 using Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: true)
+    .AddEnvironmentVariables()
+    .AddUserSecrets<Program>(optional: true);
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.ConfigureService();
+builder.Services.ConfigureService(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ Services/MCPClients/YouTubeMusicClient.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using ModelContextProtocol.Client;
+
+namespace Services.MCPClients
+{
+    public class YouTubeMusicClient : IMCPClientBuilder
+    {
+        private readonly IMcpClient _client;
+        public YouTubeMusicClient(IConfiguration configuration)
+        {
+            _client = McpClientFactory.CreateAsync(new StdioClientTransport(new()
+            {
+                Name = "YouTubeMusic",
+                Command = "npx",
+                Arguments = ["-y", "@instructa/mcp-youtube-music"],
+                EnvironmentVariables = new Dictionary<string, string>()
+                {
+                    { "YOUTUBE_API_KEY", configuration["YOUTUBE_API_KEY"]! },
+
+                }!,
+            })).Result;
+        }
+
+        public string Name => "YouTubeMusic";
+
+        ValueTask<IList<McpClientTool>> IMCPClientBuilder.GetTools() => _client.ListToolsAsync();
+
+
+    }
+}
